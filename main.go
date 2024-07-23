cc907f3f07374235f86ca2e19a4389bf2ccf24bd
Albert Skonieczny <albertskonieczny@gmail.com>
2024-07-23 17:18:28 +0200

    Add validatePrice in getFlatPricesFromSite

--- main.go
+++ main.go
@@ -2,8 +2,37 @@ package main
 
 import (
 	"io"
+	"log"
 	"os"
+	"unicode"
 )
+
+func getFlatPricesFromSite(textContent string) {
+	const currNotation = "zł"
+
+	currNotationStartIndexes := startIndexStrOccurs(textContent, currNotation)
+	if len(currNotationStartIndexes) == 0 {
+		log.Printf("No `%s` has been found on site with textContent[:100] of %s", currNotation, textContent[:100])
+	}
+
+	prices := make([]string, 0)
+
+	validatePrice := func(s string) (bool, string) {
+		price := make([]rune, 0)
+		for _, r := range s {
+			switch {
+			case unicode.IsSpace(r):
+				continue
+			case unicode.IsDigit(r):
+				price = append(price, r)
+			default:
+				return false, ""
+			}
+		}
+		return len(price) > 0, string(price)
+	}
+
+}
 
 func main() {
 	sp := NewScraper()
