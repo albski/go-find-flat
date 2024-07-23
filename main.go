a5fb7e86f10b86f48dc40cbea35b174f778b72bc
Albert Skonieczny <albertskonieczny@gmail.com>
2024-07-23 18:29:32 +0200

    Add working getFlatPricesFromSite in main

--- main.go
+++ main.go
@@ -1,21 +1,19 @@
 package main
 
 import (
-	"io"
+	"bytes"
+	"fmt"
 	"log"
-	"os"
 	"unicode"
 )
 
-func getFlatPricesFromSite(textContent string) {
+func getFlatPricesFromSite(textContent string) []string {
 	const currNotation = "zł"
 
 	currNotationStartIndexes := startIndexStrOccurs(textContent, currNotation)
 	if len(currNotationStartIndexes) == 0 {
 		log.Printf("No `%s` has been found on site with textContent[:100] of %s", currNotation, textContent[:100])
 	}
-
-	prices := make([]string, 0)
 
 	validatePrice := func(s string) (bool, string) {
 		price := make([]rune, 0)
@@ -29,13 +27,47 @@ func getFlatPricesFromSite(textContent string) {
 				return false, ""
 			}
 		}
-		return len(price) > 0, string(price)
+		return true, string(price)
 	}
 
+	textContentRunes := []rune(textContent)
+	prices := make([]string, 0)
+	for _, idx := range currNotationStartIndexes {
+		i := 0
+		p := ""
+		for {
+			i++
+			if idx-i < 0 {
+				break
+			}
+
+			tp := textContentRunes[idx-i : idx]
+			bp, vp := validatePrice(string(tp))
+			if !bp {
+				break
+			}
+
+			p = vp
+		}
+
+		if len(p) == 0 {
+			continue
+		}
+
+		prices = append(prices, p)
+	}
+
+	return prices
 }
 
 func main() {
 	sp := NewScraper()
 	rdr := sp.getTextContent("https://www.olx.pl/nieruchomosci/mieszkania/poznan/q-polanka/?search%5Bfilter_float_price%3Ato%5D=2400&search%5Border%5D=created_at%3Adesc")
-	io.Copy(os.Stdout, rdr)
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(rdr)
+
+	str := buf.String()
+
+	prices := getFlatPricesFromSite(str)
+	fmt.Println(prices)
 }
